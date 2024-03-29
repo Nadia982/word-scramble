921a9a86bb59842b0025435bbfa204bbb8f7cf27
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-29 00:04:32 +0000

    added words from spelling rule 38 and 39

---
+++ js/words.js
@@ -0,0 +1,77 @@
+const words = [
+    {word: "forgotten",
+hint: "something lost from memory or not remembered anymore."
+}, 
+{word: "forgetting",
+hint: "when you are not remembering something, you are ______ it."
+}, 
+{
+    word: "beginner",
+    hint: "someone who is not experienced at doing something."
+},
+{
+    word: "beginning",
+    hint: "the start of something."
+},
+{
+    word: "preferred",
+    hint: "something liked more than other things or chosen as the best option."
+},
+{
+    word: "preferring",
+    hint: "choosing one thing in preference to another because you like it more."
+},
+{
+    word: "gardening",
+    hint: "this can include planting flowers and mowing the lawn"
+},
+{
+    word: "limiting",
+    hint: "setting boundaries or restrictions that control what you can do."
+},
+{
+    word: "gym",
+    hint: "a place where people can go to work out."
+},
+{
+    word: "myth",
+    hint: "an old story that might not be real but teaches important lessons."
+},
+{
+    word: "mystery",
+    hint: "a puzzling situation or event that needs to be solved or understood."
+},
+{
+    word: "symbol",
+    hint: "something that represents or stands for an idea, feeling, or thing."
+},
+{
+    word: "synonym",
+    hint: "a word that means the same or nearly the same as another."
+},
+{
+    word: "system",
+    hint: "a routine or plan where different parts work together to make something happen smoothly, like a school schedule."
+},
+{
+    word: "physical",
+    hint: "involving movements, actions, or objects you can see, feel, or touch in the real world."
+},
+{
+    word: "crystal",
+    hint: "A shiny, clear rock that forms in shapes and reflects light beautifully, often used for jewelry or decorations."
+},
+{
+    word: "rhythm",
+    hint: "a pattern of beats or sounds that repeat in music or poetry."
+},
+
+
+// {
+//     word: "",
+//     hint: ""
+// },
+
+
+
+]
