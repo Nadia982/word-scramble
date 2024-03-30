a7aec56b69718d4ea02b636302c980ec97f8679c
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-30 13:25:36 +0000

    added text to speech functionality to 'read word' and 'read definition' buttons.

--- js/script.js
+++ js/script.js
@@ -2,25 +2,30 @@ const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
 timeText = document.querySelector(".time b"),
 inputField = document.querySelector(".input"),
+readWordBtn = document.querySelector("#read-word-btn"),
+readDefBtn = document.querySelector("#read-definition-btn"),
 refreshBtn = document.querySelector(".refresh-word"),
+voiceSelect = document.querySelector("#voice-select"),
+body = document.querySelector("body"),
 checkBtn = document.querySelector(".check-word");
 let message = document.querySelector(".message");
 let scoreContainer = document.querySelector(".score-container"); 
-let correctWord, timer;
+let correctWord, timer, wordDefinition;
 let score = 0;
 
 const initGame = () => { 
+    
     let randomObj = words [Math.floor(Math.random() * words.length)];
     let wordArray = randomObj.word.split("");
     for(let i=wordArray.length-1; i>0; i--) {
-        // console.log(`i is ${i}`)
         let j = Math.floor(Math.random() * (i+1));
-        // console.log(`j is ${j}`)
         [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
     }
     wordText.innerText = wordArray.join("");
-    hintText.innerText = randomObj.hint;
+    hintText.innerText = randomObj.definition;
     correctWord = randomObj.word.toLocaleLowerCase();
+    wordDefinition = randomObj.definition.toLocaleLowerCase();
+    console.log(correctWord);
     inputField.value = "";
     inputField.setAttribute("Maxlength", correctWord.length )
     message.innerHTML = "";
@@ -50,6 +55,7 @@ const checkWord = () => {
         message.classList.add("correct");
         message.innerHTML = `Well done - you spelt '${correctWord}' correctly!`;
         refreshBtn.classList.remove("hide");
+        refreshBtn.focus();
         score++;
         scoreContainer.innerHTML = score;
     } 
@@ -57,3 +63,70 @@ const checkWord = () => {
 
 refreshBtn.addEventListener("click", initGame);
 checkBtn.addEventListener("click", checkWord);
+
+//Initialise SpeechSynthesis API
+const synth = window.speechSynthesis;
+
+//Initialise the voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  //Loop through voices and create an option for each one
+  voices.forEach((voice) => {
+    //Create option element for each voice
+    const option = document.createElement("option");
+    //Fill option with voice and language
+    option.textContent = voice.lang + " (" + voice.name + ")";
+    //Set required option attributes
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
+    option.style.fontSize = "0.9rem";
+  });
+};
+
+getVoices();
+
+//the following code is required in order to get the voices.
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+//Speak
+const speak = (whatToSay) => {
+  //Check if already speaking
+  if (synth.speaking) {
+    console.error("Already speaking...");
+    return;
+  }
+  if (correctWord !== "") {
+    //Get text to speak
+    const speakText = new SpeechSynthesisUtterance(whatToSay);
+
+    //Speak end
+    speakText.onend = (e) => {
+      console.log("Finished speaking");
+    };
+    //Speak error
+    speakText.onerror = (e) => {
+      console.error("Something went wrong");
+    };
+    //Determining which voice to use to speak
+    const selectedVoice = "en-US";
+
+    //Speak
+    synth.speak(speakText);
+  }
+};
+
+//Event listeners
+// Text form submission
+readWordBtn.addEventListener("click", (e) => {
+  speak(correctWord);
+});
+
+readDefBtn.addEventListener("click", (e) => {
+    speak(wordDefinition);
+  });
--- js/words.js
+++ js/words.js
@@ -1,212 +1,212 @@
 const words = [
     {word: "forgotten",
-hint: "something lost from memory or not remembered anymore."
+definition: "something lost from memory or not remembered anymore."
 }, 
 {word: "forgetting",
-hint: "when you are not remembering something, you are ______ it."
+definition: "when you are not remembering something, you are ______ it."
 }, 
 {
     word: "beginner",
-    hint: "someone who is not experienced at doing something."
+    definition: "someone who is not experienced at doing something."
 },
 {
     word: "beginning",
-    hint: "the start of something."
+    definition: "the start of something."
 },
 {
     word: "preferred",
-    hint: "something liked more than other things or chosen as the best option."
+    definition: "something liked more than other things or chosen as the best option."
 },
 {
     word: "preferring",
-    hint: "choosing one thing in preference to another because you like it more."
+    definition: "choosing one thing in preference to another because you like it more."
 },
 {
     word: "gardening",
-    hint: "an outdoor activity that can include planting flowers and mowing the lawn."
+    definition: "an outdoor activity that can include planting flowers and mowing the lawn."
 },
 {
     word: "limiting",
-    hint: "setting boundaries or restrictions that control what you can or can't do."
+    definition: "setting boundaries or restrictions that control what you can or can't do."
 },
 {
     word: "gym",
-    hint: "a place where people can go to work out."
+    definition: "a place where people can go to work out."
 },
 {
     word: "myth",
-    hint: "a story, often from long ago, that might not be real but teaches important lessons."
+    definition: "a story, often from long ago, that might not be real but teaches important lessons."
 },
 {
     word: "mystery",
-    hint: "a puzzling situation or event that needs to be solved or understood."
+    definition: "a puzzling situation or event that needs to be solved or understood."
 },
 {
     word: "symbol",
-    hint: "something that represents or stands for an idea, feeling, or thing."
+    definition: "something that represents or stands for an idea, feeling, or thing."
 },
 {
     word: "synonym",
-    hint: "a word that means the same or nearly the same as another."
+    definition: "a word that means the same or nearly the same as another."
 },
 {
     word: "system",
-    hint: "a routine or plan where different parts work together to make something happen smoothly, like a school schedule."
+    definition: "a routine or plan where different parts work together to make something happen smoothly, like a school schedule."
 },
 {
     word: "physical",
-    hint: "involving movements, actions, or objects you can see, feel, or touch in the real world."
+    definition: "involving movements, actions, or objects you can see, feel, or touch in the real world."
 },
 {
     word: "crystal",
-    hint: "A shiny, clear rock that forms in shapes and reflects light beautifully, often used for jewelry or decorations."
+    definition: "A shiny, clear rock that forms in shapes and reflects light beautifully, often used for jewelry or decorations."
 },
 {
     word: "rhythm",
-    hint: "a pattern of beats or sounds that repeat in music or poetry."
+    definition: "a pattern of beats or sounds that repeat in music or poetry."
 },
 {
     word: "young",
-    hint: "antonym of 'old'"
+    definition: "antonym of 'old'"
 },
 
 {
     word: "double",
-    hint: "twice as much or having two of something."
+    definition: "twice as much or having two of something."
 },
 
 {
     word: "country",
-    hint: "a place where people live, with its own government, land, and culture."
+    definition: "a place where people live, with its own government, land, and culture."
 },
 {
     word: "encourage",
-    hint: "to give support or praise to someone to help them feel confident and motivated."
+    definition: "to give support or praise to someone to help them feel confident and motivated."
 },
 
 {
     word: "borough",
-    hint: "a small town or district within a larger city, like a neighborhood with its own identity."
+    definition: "a small town or district within a larger city, like a neighborhood with its own identity."
 },
 {
     word: "enormous",
-    hint: "exceptionally large, like a colossal mountain or immense skyscraper."
+    definition: "exceptionally large, like a huge crocodile or an immense skyscraper."
 },
 {
     word: "enough",
-    hint: "a sufficient amount."
+    definition: "a sufficient amount."
 },
 
 {
     word: "tough",
-    hint: "hard to do or endure, like a challenge that requires strength, effort, or resilience."
+    definition: "hard to do or endure, or a person or thing that shows strength, effort, or resilience. "
 },
 
 {
     word: "colour",
-    hint: "a shade, hue or tint such as red, blue or green."
+    definition: "a shade, hue or tint such as red, blue or green."
 },
 
 {
     word: "favour",
-    hint: "an act of kindness or help shown to someone, like doing a task that someone has asked you to do."
+    definition: "an act of kindness or help shown to someone, like doing a task that someone has asked you to do."
 },
 
 {
     word: "flavour",
-    hint: "the taste of something, like sweetness, sourness, or spiciness, making food enjoyable."
+    definition: "the taste of something, like sweetness, sourness, or spiciness, making food enjoyable."
 },
 
 {
     word: "humour",
-    hint: "a funny or amusing quality that makes people laugh or smile, found in jokes or situations."
+    definition: "a funny or amusing quality that makes people laugh or smile, found in jokes or situations."
 },
 
 // {
 //     word: "information",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "preparation",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "admiration",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "transportation",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "adoration",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "action",
-//     hint: ""
+//     definition: ""
 // },
 
 // {
 //     word: "reaction",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "fiction",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "solution",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "conclusion",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "invention",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "decision",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "direction",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "pollution",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "exploration",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "admission",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "expansion",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "construction",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "emotion",
-//     hint: ""
+//     definition: ""
 // },
 // {
 //     word: "communication",
-//     hint: ""
+//     definition: ""
 // },
 
 
