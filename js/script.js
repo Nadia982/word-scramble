e0f737bea874bf2a08fe7f4db2e12cab9eb4c73f
Natalie Gillam <ngillam@hotmail.co.uk>
2024-05-06 15:05:17 +0100

    replaced 'innerHTML' with 'innerText'

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ window.onload = function () {
     let progressBar = document.querySelector(".slider progress");
     progressBar.value = slider.value;
     let sliderValue = document.querySelector(".slider-value");
-    sliderValue.innerHTML = slider.value;
+    sliderValue.innerText = slider.value;
   };
 };
 
@@ -100,7 +100,7 @@ const speakButton = (whatToSay) => {
 
 const initGame = () => {
   triesLeft = 3;
-  totalWords.innerHTML = `${words.length}`;
+  totalWords.innerText = `${words.length}`;
   let randomObj = words[Math.floor(Math.random() * words.length)];
 
   const scrambleWord = (randomObj) => {
@@ -121,20 +121,20 @@ const initGame = () => {
   correctWord = randomObj.word;
   wordDefinition = randomObj.definition;
   inputField.value = "";
-  inputSpan.innerHTML = `Type the correctly spelt word here`;
+  inputSpan.innerText = `Type the correctly spelt word here`;
   inputField.classList.remove("correct");
   inputSpan.classList.remove("correct");
   inputField.setAttribute("Maxlength", correctWord.length + 5);
-  message.innerHTML = "";
-  emoji.innerHTML = "";
+  message.innerText = "";
+  emoji.innerText = "";
   newWordBtn.classList.add("hide");
   inputField.focus();
   checkBtn.classList.remove("hide");
   if (randomObj.spelling_rule === 61) {
-    homophone.innerHTML = `This word is a homophone or near-homophone`;
+    homophone.innerText = `This word is a homophone or near-homophone`;
     homophone.classList.add("highlight");
   } else {
-    homophone.innerHTML = ``;
+    homophone.innerText = ``;
     homophone.classList.remove("highlight");
   }
 
@@ -163,18 +163,18 @@ const initGame = () => {
 inputField.addEventListener("input", function () {
   if (inputField.value.length > correctWord.length) {
     message.classList.add("incorrect");
-    inputSpan.innerHTML = `The word contains only ${correctWord.length} letters`;
+    inputSpan.innerText = `The word contains only ${correctWord.length} letters`;
     inputSpan.classList.add("incorrect");
     inputField.classList.add("incorrect");
   } else if (inputField.value.length <= correctWord.length && triesLeft > 0) {
-    message.innerHTML = ``;
+    message.innerText = ``;
     inputSpan.classList.remove("incorrect");
-    inputSpan.innerHTML = `Type the correctly spelt word here`;
+    inputSpan.innerText = `Type the correctly spelt word here`;
     inputField.classList.remove("incorrect");
   } else {
     inputSpan.classList.remove("incorrect");
     inputField.classList.remove("incorrect");
-    inputSpan.innerHTML = `The word is '${correctWord}' - please type it below to continue.`
+    inputSpan.innerText = `The word is '${correctWord}' - please type it below to continue.`
   }
 });
 
@@ -188,11 +188,11 @@ const checkWord = () => {
     message.classList.add("incorrect");
     triesLeft--;
     if (triesLeft > 1) {
-      inputSpan.innerHTML = `Not quite - please try again! You have ${triesLeft} guesses left.`;
+      inputSpan.innerText = `Not quite - please try again! You have ${triesLeft} guesses left.`;
     } else if (triesLeft === 1) {
-      inputSpan.innerHTML = `Not quite - please try again! You have one guess left.`;
+      inputSpan.innerText = `Not quite - please try again! You have one guess left.`;
     } else {
-      inputSpan.innerHTML = `The word is '${correctWord}' - please type it below to continue.`;
+      inputSpan.innerText = `The word is '${correctWord}' - please type it below to continue.`;
     }
 
     inputField.classList.add("incorrect");
@@ -227,14 +227,14 @@ const checkWord = () => {
     message.classList.remove("incorrect");
     message.classList.add("correct");
     inputField.classList.add("correct");
-    emoji.innerHTML = `${emojis[randomEmojiIndex]}`;
-    inputSpan.innerHTML = `Well done &#8211; you spelt '${correctWord}' correctly!`;
+    emoji.innerText = `${emojis[randomEmojiIndex]}`;
+    inputSpan.innerText = `Well done &#8211; you spelt '${correctWord}' correctly!`;
     inputSpan.classList.add("correct");
     newWordBtn.classList.remove("hide");
     newWordBtn.focus();
     score++;
     checkBtn.classList.add("hide");
-    resultsSoFar.innerHTML = `${score}`;
+    resultsSoFar.innerText = `${score}`;
   }
 };
 
